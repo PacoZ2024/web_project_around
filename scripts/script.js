b967d0030025545b9828447a28c477647a18e513
Megaman <pacosclub@gmail.com>
2025-02-09 12:53:19 -0600

    Function to send the form

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,35 @@
-let content = document.querySelector(".content");
-let form = document.querySelector(".form");
 let popup = document.querySelector(".popup");
 
+let content = document.querySelector(".content");
 let editButton = content.querySelector(".content__profile-edit-button");
 let nameProfile = content.querySelector(".content__profile-name");
 let aboutMe = content.querySelector(".content__about-me");
 
+let form = document.querySelector(".form");
 let closeButton = form.querySelector(".form__label-close-button");
 let fieldName = form.querySelector(".form__field-name");
 let fieldAboutMe = form.querySelector(".form__field-about-me");
+let saveButton = form.querySelector(".form__save-button");
 
-editButton.addEventListener("click", function () {
+function handleOpenPopup() {
   popup.classList.add("popup__open");
   fieldName.value = nameProfile.textContent;
   fieldAboutMe.value = aboutMe.textContent;
-});
+}
 
-closeButton.addEventListener("click", function () {
+function handleClosePopup() {
   popup.classList.remove("popup__open");
-});
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = fieldName.value;
+  aboutMe.textContent = fieldAboutMe.value;
+  handleClosePopup();
+}
+
+editButton.addEventListener("click", handleOpenPopup);
+
+closeButton.addEventListener("click", handleClosePopup);
+
+saveButton.addEventListener("submit", handleProfileFormSubmit);
